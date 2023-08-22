660f757ff189b1a10921a75fda9685b0f429bdb4
Bortoli94 <bortoli.rosario@gmail.com>
2023-08-22 15:51:38 -0300

    Ejercicio 1 y 2
    Ejercicio 1 y 2 finalizados

---
+++ ExtensionMethods.Exceptions.Unit Test/ExceptionsPractica.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Exceptions.Unit_Test
+{
+    internal class ExceptionsPractica
+    {
+        public static void DividirPorCero(int i)
+        {
+            try
+            {
+                int dividir = i / 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Ha finalizado el primer ejercicio");
+            }
+        }
+
+        public static void Dividir(int dividendo, int divisor)
+        {
+            try
+            {
+                float resultado = dividendo / divisor;
+                Console.WriteLine($"{dividendo} / {divisor} = {resultado}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("¡Solo Chuck Norris divide por cero!");
+            }
+        }
+    }
+}
---
+++ ExtensionMethods.Exceptions.Unit Test/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods.Exceptions.Unit_Test
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numConvertido;
+            string numIngresado;
+
+            Console.WriteLine("Ejercicio 1");
+            Console.Write("Ingrese un numero: ");
+            numIngresado = Console.ReadLine();
+            while (!int.TryParse(numIngresado, out numConvertido))
+            {
+                Console.Write("Ingrese un numero valido: ");
+                numIngresado = Console.ReadLine();
+            }
+            ExceptionsPractica.DividirPorCero(numConvertido);
+
+
+            Console.WriteLine("\nEjercicio 2");
+            try
+            {
+                Console.Write("Ingrese numero dividendo: ");
+                numIngresado = Console.ReadLine();
+                numConvertido = Convert.ToInt32(numIngresado);
+                
+                Console.Write("Ingrese numero divisor: ");
+                string numIngresado2 = Console.ReadLine();
+                int numConvertido2 = Convert.ToInt32(numIngresado2);
+                
+                ExceptionsPractica.Dividir(numConvertido, numConvertido2);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("¡Seguro Ingreso una letra o no ingreso nada!");
+            }
+            finally
+            {
+                Console.WriteLine("Ha finalizado el segundo ejercicio");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
