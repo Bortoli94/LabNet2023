7970a9eea8a1c004a124fbb6db5f8602b53ac48a
Bortoli94 <bortoli.rosario@gmail.com>
2023-09-13 23:44:58 -0300

    Se agrego validaciones al MVC de la practica 6

--- Practica.EF/Practica.EF.Entities/DTO/CategoryDto.cs
+++ Practica.EF/Practica.EF.Entities/DTO/CategoryDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Practica.EF.Entities.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica.EF.Entities.DTO
 {
     public class CategoryDto
     {
         public int CategoryID { get; set; }
+        
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public string CategoryName { get; set; }
         public string Description { get; set; }
     }
--- Practica.EF/Practica.EF.Entities/DTO/ShipperDto.cs
+++ Practica.EF/Practica.EF.Entities/DTO/ShipperDto.cs
@@ -1,8 +1,12 @@
-﻿namespace Practica.EF.Entities.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Practica.EF.Entities.DTO
 {
     public class ShipperDto
     {
         public int ShipperID { get; set; }
+        
+        [Required(ErrorMessage = "El campo es obligatorio")]
         public string CompanyName { get; set; }
         public string Phone { get; set; }
     }
